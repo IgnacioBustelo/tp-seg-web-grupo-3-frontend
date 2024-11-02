a5e1addde3ce32a6f4d3d5ea8924c2fbc4d38576
ignacio-bustelo <ignacio.bustelo@despegar.com>
2024-11-02 03:01:43 -0300

    added authToken default

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import {store} from "../store/store";
 
+const authToken = store.getState().auth.authToken;
 export const api = axios.create({
-  //TODO completar cuando se tengan los endpoints del back
-  baseURL: "http://localhost:8080/",
+    //TODO completar cuando se tengan los endpoints del back
+    baseURL: "http://localhost:8080/", headers: {
+        'Authorization': 'Bearer ' + authToken,
+    }
 });
